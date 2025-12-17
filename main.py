bb9bd0bd549284ea2667a2f23f6f2d150885b791
Alejandro Ouslan <alejandro.ouslan@upr.edu>
2025-12-17 11:21:09 -0400

    update precomit and update the minimum versions

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
-import hashlib
-import os
+import jp_tools
 
 
 def main():
-    for filename in os.listdir("dist/"):
-        if filename.endswith(".tar.gz"):
-            file_path = os.path.join("dist/", filename)
-            print(f"{filename}: {sha256sum(file_path)}")
-
-
-def sha256sum(file_path: str, chunk_size: int = 8192) -> str:
-    sha256 = hashlib.sha256()
-    with open(file_path, "rb") as f:
-        for chunk in iter(lambda: f.read(chunk_size), b""):
-            sha256.update(chunk)
-    return sha256.hexdigest()
+    jp_tools.download(
+        url="https://raw.githubusercontent.com/gitinference/jp-tools/refs/heads/main/LICENSE",
+        filename="DELETE.txt",
+    )
 
 
 if __name__ == "__main__":
